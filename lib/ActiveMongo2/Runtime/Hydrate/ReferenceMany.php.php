b4281422fef497ff6dcd49b117cc79263f8bed7c
César D. Rodas <crodas@php.net>
2012-12-17 19:21:56 -0300

    foobar

---
+++ lib/ActiveMongo2/Runtime/Hydrate/ReferenceMany.php.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace ActiveMongo2\Runtime\Hydrate;
+
+use ActiveMongo2\Runtime\Reference as zReference;
+use ActiveMongo2\Runtime\Serialize;
+
+class ReferenceMany
+{
+    public static function Hydrate($values, $ann, $connection)
+    {
+        if (!is_array($values)) {
+            return NULL;
+        }
+
+        $class = current($ann['args']);
+        $class = $connection->getDocumentClass($class);
+
+        $docs = array();
+        $map  = Serialize::getDocummentMapping($class);
+        foreach ($values as $id => $value) {
+            $doc[$id] = new zReference($value, $class, $connection, $map);
+        }
+
+        return $docs;
+    }
+}
