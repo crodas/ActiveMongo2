e1e1b02b76f1de2a9cffaf32d8f3fa35e18a545a
César D. Rodas <crodas@php.net>
2013-05-05 03:47:07 -0400

    Compile mongo driver in travis

--- travis/mongo.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -x 
-
-VERSION=1.3.7
-
-wget http://pecl.php.net/get/mongo-$VERSION.tgz
-tar -xzf mongo-$VERSION.tgz
-sh -c "cd mongo-1.2.10 && phpize && ./configure && sudo make install"
