e5ba9d9074c82e50608d1f97c7dd516c9ec2c98f
César D. Rodas <crodas@php.net>
2014-05-07 18:24:29 -0400

    testing index;

--- tests/IndexTest.php
+++ tests/IndexTest.php
@@ -12,6 +12,11 @@ class IndexTest extends \phpunit_framework_testcase
         $conn = getConnection();
         $conn->ensureIndex();
 
+        $indexes = $conn->user_posts->rawCollection()->getIndexInfo();
+
+        $this->assertEquals(2, count($indexes));
+        $this->assertEquals(true,  $indexes[1]['unique']);
+
         $post = new PostNoTitleDocument;
         $post->uri = "something that I say";
         $conn->save($post);
